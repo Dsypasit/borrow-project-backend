573ae66b0ffe42c67acad5d2ecbc4d3df1451653
dsypasit <ong.pasit@gmail.com>
2023-03-11 18:15:33 +0700

    fix: undo function deleted

--- src/controllers/source.controller.ts
+++ src/controllers/source.controller.ts
@@ -1,16 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import { sourceVaild } from '../utils/validation';
 
 const prisma = new PrismaClient();
 
 export async function getSources(req: Request, res: Response) {
   const source = await prisma.source.findMany();
-  res.json(source);
+  res.status(200).json(source);
 }
 
 export async function createSource(req: Request, res: Response) {
-  if (req.body.name === undefined) {
-    res.json({
+  if (!sourceVaild(req.body)) {
+    res.status(400).json({
       message: 'create source error',
       body: req.body,
     });
@@ -21,18 +22,18 @@ export async function createSource(req: Request, res: Response) {
       name: req.body.name,
     },
   });
-  res.json(result);
+  res.status(201).json(result);
 }
 
 export async function deleteSource(req: Request, res: Response) {
   const { id } = req.params;
   if (id === undefined) {
-    res.json({
+    res.status(400).json({
       message: "can't delete Source",
     });
   }
   const result = await prisma.source.delete({
     where: { id: Number(id) },
   });
-  res.json(result);
+  res.status(200).json(result);
 }
