74b45477e3fe004ad47b380a953a054cb33a3332
VasapolRittideah <vasapol.rittideah@outlook.com>
2023-03-12 17:07:31 +0700

    fix: fix function name typo

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import {
   getProducts,
   getProductById,
-  createProducts,
+  createProduct,
   deleteProduct,
   updateFrequency,
 } from '../controllers/product.controller';
@@ -11,7 +11,7 @@ const route = express.Router();
 
 route.get('/', getProducts);
 route.get('/:id', getProductById);
-route.post('/', createProducts);
+route.post('/', createProduct);
 route.put('/frequency/:id', updateFrequency);
 route.delete('/:id', deleteProduct);
 
