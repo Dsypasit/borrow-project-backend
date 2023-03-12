628f25e8db80cef9a3e792ff4a420f9a69656064
dsypasit <ong.pasit@gmail.com>
2023-03-12 20:39:20 +0700

    feat: add seed item

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
+import fs from 'fs'
 
 const prisma = new PrismaClient();
 
@@ -26,8 +27,83 @@ async function main() {
   await createRoom();
   await createSource();
   await createProducts();
+  await createProductItems();
+}
+
+async function createProductItems(){
+  let data = JSON.parse(fs.readFileSync('prisma/data.json', 'utf8'));
+  for(var i=0; i<data.length;i++){
+    let source = await checkSource(data[i].source)
+    let product = await checkProduct(data[i].Product)
+    console.log(i)
+    try{
+      await prisma.productItem.create({
+        data: {
+          serialNumber: String(data[i].serialRef),
+          productId: Number(product?.id),
+          roomId: 1,
+          sourceId: Number(source?.id),
+        },
+      })
+    }catch(e){
+      if(e instanceof Prisma.PrismaClientKnownRequestError){
+        if(e.code === 'P2002'){
+          continue
+        }
+      }
+    }
+    
+  }
+}
+
+async function checkSource(s:any){
+  let source = await prisma.source.findFirst({
+    where: {
+      name: s,
+    }
+  });
+  if (source === null){
+    source = await prisma.source.create({
+      data:{
+        name: s
+      }
+    })
+  } 
+  return source
+}
+
+// async function checkCategory(s:any){
+//   let source = await prisma.source.findFirst({
+//     where: {
+//       name: s,
+//     }
+//   });
+//   if (source === null){
+//     source = await prisma.source.create({
+//       data:{
+//         name: s
+//       }
+//     })
+//   } 
+//   return source
+// }
+
+async function checkProduct(p:any){
+  let product = await prisma.product.findFirst({
+    where: {
+      name: p,
+    }
+  });
+  if (product === null){
+    product = await prisma.product.create({
+      data:{
+        name: p
+      }
+    })
+  } 
+  return product
 }
 
 main()
-  .catch((e) => console.error(e))
-  .finally(() => prisma.$disconnect());
+.catch((e) => console.error(e))
+.finally(() => prisma.$disconnect());
